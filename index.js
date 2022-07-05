d32cdca201f4bfb1076f9f34b5d897e7e1d78c6b
chuongnh4499 <nghuuchuong4499@gmail.com>
2022-07-05 14:20:18 +0700

    move lib to dev dependencies

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const morgan = require('morgan')
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 123')
 })
 
 app.listen(port, () => {
